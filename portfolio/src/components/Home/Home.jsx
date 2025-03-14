430c13e4ac62607ead2feaccf0547437454f621a
Ravikumar Jha <jharavi153@gmail.com>
2025-03-14 15:21:19 +0530

    Add Animation

--- portfolio/src/components/Home/Home.jsx
+++ portfolio/src/components/Home/Home.jsx
@@ -1,26 +1,52 @@
-import React from 'react';
+import { useEffect, useRef } from "react";
 import "./Home.css";
 import profile from "../../assets/profile.jpg";
-
-
+import { Typewriter } from "react-simple-typewriter";
 
 function Home() {
   return (
     <div id="home">
-      <div className='left'>
-        <div className='details'>
-          <div className='line1'>I'm </div>
-          <div className='line2'>Ravikumar Jha</div>
-          <div className='line3'>Front-End Developer</div>
+      <div className="left">
+        <div className="details">
+          <div className="line1">I'm </div>
+          <div className="line2">Ravikumar Jha</div>
+          <span className="typing-effect">
+            <Typewriter
+              words={[
+                "Frontend Developer 🚀",
+                "React Enthusiast 💻",
+                "UI/UX Passionate 🎨",
+                "Building Scalable Web Apps 🌐",
+              ]}
+              loop={true} // Infinite loop
+              typeSpeed={50} // Speed of typing
+              deleteSpeed={40} // Speed of deleting
+              delaySpeed={1000}
+              size={100} // Pause before retyping
+            />
+          </span>
+
+          <div className="des">
+            <Typewriter
+              words={[
+                "I am convinced that technology has the ability to turn concepts into tangible results. As a Frontend Developer, my focus is on building engaging and user-friendly web applications utilizing React and JavaScript. My enthusiasm for technology enhances my analytical skills, allowing me to create solutions that are both practical and user-friendly. I take pleasure in working with teams, exploring new technologies, and expanding the limits of web development.",
+              ]}
+              loop={1} // Runs only once
+              typeSpeed={50} // Typing speed
+              deleteSpeed={999999} // Prevent deletion
+              delaySpeed={999999} // Prevent retyping
+            />
+          </div>
+
           <button>Hire me</button>
         </div>
       </div>
-      <div className='right'>
-        <img src = {profile}  alt = "profile-image"/>
+
+      <div className="right">
+        <img src={profile} alt="profile-image" />
       </div>
-      
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
