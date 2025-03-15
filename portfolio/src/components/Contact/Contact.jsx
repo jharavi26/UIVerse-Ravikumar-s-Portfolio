3cc630ec4e32393e5acb9ac83a7722f687819ac6
Ravikumar Jha <jharavi153@gmail.com>
2025-03-15 16:30:23 +0530

    Add Animation2

--- portfolio/src/components/Contact/Contact.jsx
+++ portfolio/src/components/Contact/Contact.jsx
@@ -4,17 +4,31 @@ import contact from "../../assets/Contact.png"
 
 function Contact() {
   return (
+    <>
+    <h1 className='header'>Get in Touch </h1>
     <div id = "contact">
+       
       <div className='leftside'>
         <img src = {contact} alt = "contact" />
 
       </div>
 
       <div className='rightside'>
+        <form action = "#" className='form'>
+       
+          <input type='text' name = "UserName" placeholder='Name'/>
+          <input type = "email" name = "Email" placeholder = "Email"/>
+          <textarea name = "message" id = "textarea" placeholder='Enter Your Message'/>
+          <button type='submit' id = "btn">Submit</button>
+
+          
+
+        </form>
         
         </div>
       
     </div>
+    </>
   )
 }
 
